fe57d7b79962d69db726f460b4e29989f4a6bc75
jihyun132 <jihyun.q.s@gmail.com>
2024-06-02 18:45:55 +0900

    날씨 api 완성(디자인 제외)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
+import { useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
 
 function App() {
+
+  const [city, setCity] = useState('');
+  const [result, setResult] = useState({});
+
+  const API_key = "0724e7c87275904de29ff7329d6eae99"
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`
+
+  const searchWeather = async() => {
+    try {
+      const res = await axios.get(url)
+      console.log(res)
+      setResult(res.data)
+    } catch (error) {
+      console.error(error);
+      window.alert("받아오기 실패")
+    }
+  }
+  
+
   return (
-    <div>
-    </div>
+    <AppWraper>
+      <input type="text" placeholder="도시 입력" value={city} onChange={(e)=>setCity(e.target.value)}></input>
+      <button onClick={searchWeather}>날씨 정보 확인</button>
+
+      {/* result가 빈 오브젝트가 아닐 때만 실행 */}
+      {Object.keys(result).length !==0 &&(
+      <Result>
+        <img src={result.weather[0].icon} alt="" />
+        <h2>{result.name}</h2>
+        {/* 소수점 한 자리 수까지 온도 표현 */}
+        <div>Temperature: {Math.round((result.main.temp - 273.15)*10)/10}°C</div> 
+        <div>Weather: {result.weather[0].main}</div>
+      </Result>
+        
+      )}
+
+    </AppWraper>
   );
 }
 
 export default App;
+
+
+const AppWraper = styled.div`
+width: 50%;
+margin-top: 60px;
+padding: 10px;
+`;
+
+const Result = styled.div`
+margin-top: 60px;
+padding: 10px;
+border: 1px blue solid;
+`;
