239510b58487b443da4c18298442d03f7990ccaa
tkv00 <144890194+tkv00@users.noreply.github.com>
2024-06-02 22:27:17 +0900

    헤더 로고이미지 추가

--- pocketmon/src/App.js
+++ pocketmon/src/App.js
@@ -1,6 +1,18 @@
 import styled from "styled-components";
 import "./App.css";
 import Card from "./Card";
+
+function App() {
+  return (
+    <Container>
+      <Header>
+        <Logo src="./logo.jpeg"></Logo>
+        <Title>포켓몬 도감</Title>
+      </Header>
+      <Card />
+    </Container>
+  );
+}
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -15,9 +27,10 @@ const Header = styled.header`
   right: 0;
   height: 100px;
   width: 100%;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   background-color: black;
+
 `;
 
 const Title = styled.h1`
@@ -25,19 +38,13 @@ const Title = styled.h1`
   text-align: center;
   color: white;
   font-size: 35px;
+  flex-grow: 1;
+  margin-right: 200px;
 `;
 
-function App() {
-  return (
-    <Container>
-      
-      <Header>
-        <Title>포켓몬 도감</Title>
-
-      </Header>
-      <Card />
-    </Container>
-  );
-}
+const Logo = styled.img`
+  width: 200px;
+  height: auto;
+`;
 
 export default App;
